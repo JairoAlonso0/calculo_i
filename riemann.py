a087b511496214424155006760efb9657102d5d3
JairoAlonso0 <164618606+JairoAlonso0@users.noreply.github.com>
2024-05-31 18:03:29 -0500

    Update riemann.py

--- riemann.py
+++ riemann.py
@@ -1,32 +1,37 @@
-"""calcular el área bajo la curva de la función f(x) = x^2 entre los puntos x = 2 y x = 5.
+"""Calcular el área bajo la curva de la función f(x) = x^2 entre los puntos x = 2 y x = 5.
  Determine el área utilizando el método de Riemann con 4 subintervalos usando los datos de la derecha, izquierda y centro."""
 import numpy as np
 
-a=2
-b=5
-n=4 #n coeficientes
-Δ_x=(b-a)/n
+a = 2
+b = 5
+n = 4  # número de subintervalos
+Δ_x = (b - a) / n
 
-val_der=np.linspace(a+Δ_x,b,n)
-val_izq=np.linspace(a,b-Δ_x,n)
-val_med=np.linspace(a+(Δ_x/2),b-(Δ_x/2),n)
+val_der = np.linspace(a + Δ_x, b, n)
+val_izq = np.linspace(a, b - Δ_x, n)
+val_med = np.linspace(a + (Δ_x / 2), b - (Δ_x / 2), n)
 
 def f(x):
-    x=x**2
+    x = x ** 2
     return x
 
 def riemann_izq():
-    integral=np.sum(Δ_x * f(val_izq))
-    print (f"Integral de Riemann por la izquierda: {integral} \n")
+    integral = np.sum(Δ_x * f(val_izq))
+    return integral
 
 def riemann_der():
-    integral=np.sum(Δ_x * f(val_der))
-    print (f"Integral de Riemann por la derecha: {integral} \n")
+    integral = np.sum(Δ_x * f(val_der))
+    return integral
 
 def riemann_med():
-    integral=np.sum(Δ_x * f(val_med))
-    print (f"Integral de Riemann por punto medio: {integral} \n")
+    integral = np.sum(Δ_x * f(val_med))
+    return integral
 
-riemann_der()
-riemann_izq()
-riemann_med()
+integral_der = riemann_der()
+integral_izq = riemann_izq()
+integral_med = riemann_med()
+
+print("Resultados:")
+print("Integral de Riemann por la derecha: ", integral_der)
+print("Integral de Riemann por la izquierda: ", integral_izq)
+print("Integral de Riemann por punto medio: ", integral_med)
